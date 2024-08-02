84e2aeb7298b2e3da2d4ae9f717670283323746f
prao2 <praveena_rao@intuit.com>
2024-08-02 15:05:18 -0700

    first commit

---
+++ src/main/java/com/praveena/illumino/IlluminoApplication.java
@@ -0,0 +1,13 @@
+package com.praveena.illumino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlluminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlluminoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveena/illumino/IlluminoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveena.illumino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlluminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
