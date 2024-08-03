f845c397d841ee1b53a96360b0e2ec7638b3bf17
prao2 <praveena_rao@intuit.com>
2024-08-03 09:06:16 -0700

    refactored: added a larger flowlog file and javadocs

--- src/main/java/com/illumino/parser/FlowLogDataParser.java
+++ src/main/java/com/illumino/parser/FlowLogDataParser.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public class FlowLogDataParser {
 
-    private static final String flowLogfileName = "flowlog.csv";
+    private static final String flowLogfileName = "flowlog_small.csv";//"flowlog.csv"//to test with larger data use flowlog.csv
     private static final String lookupTableFileName = "lookupTable.csv";
     public static void main(String[] args)  {
         //read flow log data from csv
@@ -24,25 +24,25 @@ public class FlowLogDataParser {
         //read lookup table data from csv
         List<LookupTableData> lookupTableDataList= readCsvData(lookupTableFileName, LookupTableData.class);
 
-        //map to hold tag based on port and protocol pair
+        //map to hold tags based on port and protocol pair
         Map<Pair<Integer, String>, String> tagLookupMap = new HashMap<>();
         //map to hold count of port and protocol combination
         Map<Pair<Integer, String>, Integer> portProtocolCountMap = new HashMap<>();
 
         //read once and populate both the maps
         lookupTableDataList.forEach(lookupTableData -> {
-            Pair<Integer, String> pair = Pair.of(lookupTableData.getDstPort(),lookupTableData.getProtocol());
+            Pair<Integer, String> pair = Pair.of(lookupTableData.getDstPort(),lookupTableData.getProtocol()!=null?  lookupTableData.getProtocol().toLowerCase(): "");
             tagLookupMap.put(pair, lookupTableData.getTag());
-            portProtocolCountMap.put(pair, portProtocolCountMap.getOrDefault(pair,0)+1);
         });
 
         //map to hold tag counts
         Map<String, Integer> tagCountMap = new HashMap<>();
 
         flowLogDataList.forEach(flowLogData -> {
-            Pair<Integer, String> pair = Pair.of(flowLogData.getDstPort(), flowLogData.getProtocol().toLowerCase());
+            Pair<Integer, String> pair = Pair.of(flowLogData.getDstPort(), flowLogData.getProtocol()!=null ?  flowLogData.getProtocol().toLowerCase(): "");
             String tag = tagLookupMap.get(pair);
             tagCountMap.put(tag, tagCountMap.getOrDefault(tag,0)+1);
+            portProtocolCountMap.put(pair, portProtocolCountMap.getOrDefault(pair,0)+1);
         });
 
         //assuming both outputs needs to written into separate csvs
@@ -71,6 +71,10 @@ public class FlowLogDataParser {
         return flowLogDataList;
     }
 
+    /**
+     * Generates tag_counts.csv from the map
+     * @param tagCountMap
+     */
     private static void writeTagsCountFile(Map<String, Integer> tagCountMap){
         try (FileWriter csvWriter = new FileWriter("tag_counts.csv")) {
             // Write header row
@@ -91,6 +95,10 @@ public class FlowLogDataParser {
         }
     }
 
+    /**
+     * Generates port_protocol_count.csv from the map
+     * @param portProtocolCountMap
+     */
     private static void writePortProtocolCountFile(Map<Pair<Integer, String>, Integer> portProtocolCountMap){
         try (FileWriter csvWriter = new FileWriter("port_protocol_count.csv")) {
             // Write header row
